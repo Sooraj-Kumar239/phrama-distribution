97e9fc6b2a4207f8314dd8066526d30b54507a12
Sooraj-Kumar239 <skoad.2194@gmail.com>
2026-03-20 21:38:24 +0000

    install mysql2 package to connect my database connection

--- server.js
+++ server.js
@@ -9,4 +9,19 @@ app.get('/', (req, res) => {
 // Server start creting server on the browser
 app.listen(3000, () => {
     console.log('Server running on port 3000');
+
+
+
+});
+// products route
+app.get('/products', (req, res)=>{
+    res.send('products List');
+});
+// Customers route
+app.get('/customers', (req, res) => {
+    res.send('Customers list');
+});
+// Server start
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
