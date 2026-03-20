6fac5e8624989cb029922f9bc16bb45a89e6508d
Sooraj-Kumar239 <skoad.2194@gmail.com>
2026-03-20 22:02:34 +0000

    there was a duplicacy app.listen removed and fixed the server this concept(dUPLICATION OF SERVER) cleared by chatgpt

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const db=require('./db') //server start and database will connect
+const db=require('./db') //server start and database will connectg
+
+
 // Home route
 app.get('/', (req, res) => {
     res.send('Pharma Distribution System Running...');
@@ -9,19 +11,14 @@ app.get('/', (req, res) => {
 // Server start creting server on the browser
 app.listen(3000, () => {
     console.log('Server running on port 3000');
-
-
-
 });
+
 // products route
 app.get('/products', (req, res)=>{
     res.send('products List');
 });
+
 // Customers route
 app.get('/customers', (req, res) => {
     res.send('Customers list');
-});
-// Server start
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
 });
