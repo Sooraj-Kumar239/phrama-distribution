fe969a4202ba2d01c20ad9dad4610725b7baea9d
Sooraj-Kumar239 <skoad.2194@gmail.com>
2026-03-20 22:35:43 +0000

    added post API to insert new record in the products table

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const db=require('./db') //server start and database will connectg
 
+app.use(express.json());//enable json body for post request
+
 
 // Home route
 app.get('/', (req, res) => {
@@ -26,6 +28,33 @@ app.get('/products', (req, res)=>{
             }
     });
 });
+
+// post request
+    app.post('/products',(req,res)=>{
+        const{
+            ProductName,
+            BatchNumber,
+            ExpirayDate,
+            StockQuantity,
+            UnitPrice,
+            RecordLevel
+        }=req.body;
+
+        const sql='insert into products'
+        (ProductName,BatchNumber,ExpirayDate,StockQuantity,UnitPrice,RecordLevel)
+        values
+        (panadol,-01-26,02-8-2028,20,20.00,00);
+
+        db.query(sql,[ProductName,BatchNumber,ExpirayDate,StockQuantity,UnitPrice,RecordLevel],
+            (err,result)=>{
+                if (err) {
+                console.log(err);
+                res.send('Error inserting product');
+            } else {
+                res.send('Product added successfully');
+            }
+            }
+        )
 
 // Customers route
 app.get('/customers', (req, res) => {
