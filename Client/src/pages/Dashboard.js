682bc6b1dd8829870118f3b06db4934d5dd4330e
Sooraj-Kumar239 <skoad.2194@gmail.com>
2026-03-31 00:32:53 +0100

    dasborad page banner designed

--- Client/src/pages/Dashboard.js
+++ Client/src/pages/Dashboard.js
@@ -6,7 +6,32 @@ function Dashboard() {
 
     return (
         <Layout>
-       
+                <div style={{
+                    width: "100%",
+                    height: "170px",   
+                    background: "linear-gradient(to right, #007bff, #00c6ff)",
+                    color: "white",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: "12px",
+                    marginBottom: "20px"
+                }}>
+
+                    <h1 style={{ margin: 0 }}>
+                        FAST ENTERPRISES
+                    </h1>
+
+                    <p style={{ margin: "5px 0" }}>
+                        Pharmaceutical Distributors
+                    </p>
+
+                    <p style={{ margin: 0 }}>
+                        "Caring for Health, Distributing with Trust"
+                    </p>
+
+                </div>
         </Layout>
     );
 }
