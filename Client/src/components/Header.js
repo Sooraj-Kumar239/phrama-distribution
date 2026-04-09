a19004208903f498350b914c5ed63456d8add5d4
Sooraj-Kumar239 <skoad.2194@gmail.com>
2026-04-09 21:42:19 +0100

    removed import in body-header.js

--- Client/src/components/Header.js
+++ Client/src/components/Header.js
@@ -1,4 +1,4 @@
-<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+{/* <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta> */}
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
